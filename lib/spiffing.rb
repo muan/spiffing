abb8c0a12bba0b3514e095e83949ea3faa0e6875
Mu-An Chiou <me@muanchiou.com>
2013-03-30 11:10:59 +0000

    Refactor

--- lib/spiffing.rb
+++ lib/spiffing.rb
@@ -1,64 +1,41 @@
 require "spiffing/version"
 
 module Spiffing
-  class CSS
 
-    def initialize file
+  class CSSConverter
 
-      begin 
-        @file = file
-        @css = File.read file
-      rescue Errno::ENOENT => e
-        @notfound = true
-        puts "- Good day. We cannot find your file, unfortunately."
-      end
+    MUGGLIFICATIONS = {
+      'colour'        => 'color',
+      'grey'          => 'gray',
+      '!please'       => '!important',
+      'transparency'  => 'opacity',
+      'centre'        => 'center',
+      'plump'         => 'bold',
+      '-photograph'   => '-image',
+      'capitalise'    => 'capitalize'
+    }
 
-      @mugglifications = {
-        'colour'        => 'color',
-        'grey'          => 'gray',
-        '!please'       => '!important',
-        'transparency'  => 'opacity',
-        'centre'        => 'center',
-        'plump'         => 'bold',
-        '-photograph'    => '-image',
-        'capitalise'    => 'capitalize'
-      }
-
+    def initialize spiffing_css_path
+      @spiffing_filepath = spiffing_css_path
+      @spiffing_css = File.read spiffing_css_path
     end
     
-    def mugglified
-
-      begin
-        @mugglifications.each do |proper_english, some_other_english|
-          @css = @css.gsub(proper_english, some_other_english)
-        end
-        create_new_css
-
-      rescue => e
-        unless @notfound
-          puts "- Good lord, something went wrong and we don't know what. We are terribly sorry, sir."
-        end
-
+    def mugglify
+      MUGGLIFICATIONS.each do |proper_english, some_other_english|
+        @normal_css = @spiffing_css.gsub(proper_english, some_other_english)
       end
 
-    end
-
-    def create_new_css
+      new_filename = File.basename( @spiffing_filepath ,'.*') + "-converted.css"
+      normal_css_file = File.new File.dirname(@spiffing_filepath) + "/" + new_filename, "w"
+      normal_css_file.write @normal_css
+      normal_css_file.close
       
-      begin
-        new_filename = File.basename( @file ,'.*') + "-converted.css"
-        new_css = File.new File.dirname(@file) + "/" + new_filename, "w"
-        new_css.write @css
-        new_css.close
-        
-        puts "- Brilliant. Your file '#{@file}' has been converted. New file name '#{new_filename}'."
+      puts "- Brilliant. Your file '#{@spiffing_filepath}' has been converted. New file name '#{new_filename}'."
 
-      rescue => e
-        puts "- Good lord, something went wrong and we don't know what. We are terribly sorry, sir."
-        
-      end
-
+    rescue
+      puts "- Something went wrong and we don't know what. We are terribly sorry."
     end
 
   end
+  
 end
--- lib/spiffing/version.rb
+++ lib/spiffing/version.rb
@@ -1,3 +1,3 @@
 module Spiffing
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
